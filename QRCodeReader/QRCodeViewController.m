7356fd9dc34d0d47ea8cd533d82476c02c5c2cac
Tsubasa Yonenaga <shawn.winghills@gmail.com>
2016-06-27 15:17:49 +0900

    customize QRCode to denote QRCode but it's not display so search the Reason and what is the means of some code

--- QRCodeReader/QRCodeViewController.m
+++ QRCodeReader/QRCodeViewController.m
@@ -7,7 +7,10 @@
 //
 
 
-@interface QRCodeViewController ()
+@interface QRCodeViewController () {
+    UIImage *_image;
+    UIImageView *_imageView;
+}
 
 @end
 
@@ -20,11 +23,23 @@
 
     
     CIFilter *ciFilter = [CIFilter filterWithName:@"CIFilter"];
-    NSData *qrData = [[NSString stringWithFormat:@""] dataUsingEncoding:NSUTF8StringEncoding];
-    [ciFilter setValue:qrData forKey:@""];
+    NSData *qrData = [[NSString stringWithFormat:@"http://nanameue.jp/?lang=ja"] dataUsingEncoding:NSUTF8StringEncoding];
+    [ciFilter setValue:qrData forKey:@"inputMessage"];
     CGImageRef cgImage = [[CIContext contextWithOptions:nil] createCGImage:[ciFilter outputImage] fromRect:[[ciFilter outputImage] extent]];
     [[CIContext contextWithOptions:nil] createCGImage:[ciFilter outputImage] fromRect:[[ciFilter outputImage] extent]];
     
+    _image = [UIImage imageWithCGImage:cgImage scale:1.0f orientation:UIImageOrientationUp];
+    UIGraphicsBeginImageContext(CGSizeMake(_allWidth,_allheight));
+    CGContextSetInterpolationQuality(UIGraphicsGetCurrentContext(), kCGInterpolationNone);
+    [_image drawInRect:CGRectMake(0, 0, _allWidth, _allheight)];
+    _image = UIGraphicsGetImageFromCurrentImageContext();
+    
+    _imageView = [UIImageView new];
+    _imageView.layer.magnificationFilter = kCAFilterNearest;
+    _imageView.frame = self.view.bounds;
+    _imageView.image = _image;
+    _imageView.contentMode = UIViewContentModeScaleAspectFit;
+    [self.view addSubview:_imageView];
 }
 
 
