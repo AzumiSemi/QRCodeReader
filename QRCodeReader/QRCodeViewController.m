5be45a2aff3a17183e666296e681308f93fb9552
Tsubasa Yonenaga <shawn.winghills@gmail.com>
2016-08-24 06:44:10 +0900

    change the code in QRCode
    http to https HP "Amazon"

--- QRCodeReader/QRCodeViewController.m
+++ QRCodeReader/QRCodeViewController.m
@@ -20,7 +20,7 @@
     [super viewDidLoad];
     
     CIFilter *ciFilter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
-    NSData *qrData = [[NSString stringWithFormat:@"http://www.souko.co.jp/"] dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *qrData = [[NSString stringWithFormat:@"https://www.amazon.co.jp/"] dataUsingEncoding:NSUTF8StringEncoding];
     [ciFilter setValue:qrData forKey:@"inputMessage"];
     CGImageRef cgImage = [[CIContext contextWithOptions:nil] createCGImage:[ciFilter outputImage] fromRect:[[ciFilter outputImage] extent]];
     [[CIContext contextWithOptions:nil] createCGImage:[ciFilter outputImage] fromRect:[[ciFilter outputImage] extent]];
